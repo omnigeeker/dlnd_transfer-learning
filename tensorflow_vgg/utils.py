cba239d4d2ab2d768643f50c6c28cdf9df2200a9
elninowang <elninowang@qq.com>
2017-06-17 18:15:08 +0800

    complete the project

--- tensorflow_vgg/utils.py
+++ tensorflow_vgg/utils.py
@@ -50,10 +50,10 @@ def load_image2(path, height=None, width=None):
         nx = width
     elif height is not None:
         ny = height
-        nx = img.shape[1] * ny / img.shape[0]
+        nx = img.shape[1] * ny // img.shape[0]
     elif width is not None:
         nx = width
-        ny = img.shape[0] * nx / img.shape[1]
+        ny = img.shape[0] * nx // img.shape[1]
     else:
         ny = img.shape[0]
         nx = img.shape[1]
@@ -61,11 +61,11 @@ def load_image2(path, height=None, width=None):
 
 
 def test():
-    img = skimage.io.imread("./test_data/starry_night.jpg")
+    img = skimage.io.imread("./test_data/puzzle.jpeg")
     ny = 300
-    nx = img.shape[1] * ny / img.shape[0]
+    nx = img.shape[1] * ny // img.shape[0]
     img = skimage.transform.resize(img, (ny, nx), mode='constant')
-    skimage.io.imsave("./test_data/test/output.jpg", img)
+    skimage.io.imsave("./test_data/output.jpg", img)
 
 
 if __name__ == "__main__":
